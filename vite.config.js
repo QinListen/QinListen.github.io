84f018a17f060ce16c240e31c09552623acc1820
Listen <66516584+QinListen@users.noreply.github.com>
2024-12-30 15:30:44 +0800

    123

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // 根路径
+  base: '/QinListen.github.io/', // 根路径
   plugins: [vue()],
   server: {
     port: 3000, // 默认开发服务器端口
@@ -11,7 +11,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src', // 设置路径别名
+      'vue': 'vue/dist/vue.esm-bundler.js',
     },
   },
 }) 
